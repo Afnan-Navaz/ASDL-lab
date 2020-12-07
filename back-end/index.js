fe41d884b43ca6e1d63b0a203d5ef6b66b59f8dd
Afnan-Navaz <afnannavaz@gmail.com>
2020-12-07 09:56:45 +0530

    added new routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const DB = require('./DB/db');
 const api = require('./routes/api');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 DB.connect((err) => {
@@ -8,8 +10,10 @@ DB.connect((err) => {
     console.log("connected to mysql");
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.use('/api', api);
 
 const port = process.env.PORT || 3000;
